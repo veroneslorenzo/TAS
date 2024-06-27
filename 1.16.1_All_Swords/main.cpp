c7c5d3c002924e2a1e50408eda08170e2b7d9d3f
veroneslorenzo <veroneslorenzo@gmail.com>
2024-06-27 21:59:03 +0200

    added trigger button

---
+++ 1.16.1_All_Swords/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main(){
+    while(1){
+        if(GetKeyState('G'))
+        {
+            break;
+        }
+    }
+    
+}
