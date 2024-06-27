1280f72efac4ce697e46db73a1e82abc31d05701
veroneslorenzo <veroneslorenzo@gmail.com>
2024-06-27 23:23:33 +0200

    implemented movement for the first chest

--- 1.16.1_All_Swords/main.cpp
+++ 1.16.1_All_Swords/main.cpp
@@ -3,12 +3,54 @@
 
 using namespace std;
 
+void firstChest();
+
 int main(){
+
     while(1){
         if(GetKeyState('G'))
         {
             break;
         }
     }
+
+    firstChest();
     
+}
+void firstChest(){
+    SetCursorPos(815, 435);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+    Sleep(10);
+    SetCursorPos(815, 681);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+    Sleep(10);
+    SetCursorPos(888, 435);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+    Sleep(10);
+    SetCursorPos(850, 681);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+    Sleep(10);
+    SetCursorPos(961, 471);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+    Sleep(10);
+    SetCursorPos(888, 681);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+    Sleep(10);
+    keybd_event('E', 0x2F, KEYEVENTF_EXTENDEDKEY | 0, 0);
+    keybd_event('E', 0x2F, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
 }
